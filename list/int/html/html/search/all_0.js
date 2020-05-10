5de25e3845ad1b1923eaeccbd8712871ca4c28c3
Federico Bolelli <federico.bolelli@unimore.it>
2020-05-10 13:40:13 +0200

    Remove old/wrong files

--- list/int/html/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['elemcompare',['ElemCompare',['../list__int_8h.html#a7268d566f05ce324f6bceea069d9793a',1,'list_int.h']]]
-];
--- list/int/html/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['list_5fint_2eh',['list_int.h',['../list__int_8h.html',1,'']]]
-];
