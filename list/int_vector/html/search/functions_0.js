c609546a134f7bb2929d96fd5b902b254af9d5b0
Travis CI User <travis@example.org>
2020-05-12 15:51:10 +0000

    Deploy code docs to GitHub Pages. Travis build: 49
    Commit: 23bfec8f9f7f58a3c17d64f4923aea09d925a4be

--- list/int_vector/html/search/functions_0.js
+++ list/int_vector/html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['elemcompare',['ElemCompare',['../list__int__vector_8c.html#a7268d566f05ce324f6bceea069d9793a',1,'list_int_vector.c']]]
+  ['createemptylist',['CreateEmptyList',['../list__int__vector_8h.html#a967d8afe585e8439bccfe995ae33e863',1,'list_int_vector.c']]]
 ];
---
+++ list/int_vector/html/search/functions_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['getheadvaluelist',['GetHeadValueList',['../list__int__vector_8h.html#aa7d9bdad51d4d8372cf5be7a4ff3e66b',1,'list_int_vector.h']]],
+  ['gettaillist',['GetTailList',['../list__int__vector_8h.html#a67e3582ac612085873c35abeaecd15c1',1,'list_int_vector.c']]]
+];
