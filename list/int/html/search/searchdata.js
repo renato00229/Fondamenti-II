46ea8b69eb621481bc26dabef8b8c3cb2fa20185
Travis CI User <travis@example.org>
2020-05-23 19:13:37 +0000

    Deploy code docs to GitHub Pages. Travis build: 73
    Commit: 355ac86d030bd6cb9229454b69e78a87a4945273

--- list/int/html/search/searchdata.js
+++ list/int/html/search/searchdata.js
@@ -1,10 +1,11 @@
 var indexSectionsWithContent =
 {
-  0: "cdegilrw",
+  0: "cdegilnrvw",
   1: "i",
   2: "l",
   3: "cdegirw",
-  4: "e"
+  4: "nv",
+  5: "e"
 };
 
 var indexSectionNames =
@@ -13,7 +14,8 @@ var indexSectionNames =
   1: "classes",
   2: "files",
   3: "functions",
-  4: "typedefs"
+  4: "variables",
+  5: "typedefs"
 };
 
 var indexSectionLabels =
@@ -22,6 +24,7 @@ var indexSectionLabels =
   1: "Classes",
   2: "Files",
   3: "Functions",
-  4: "Typedefs"
+  4: "Variables",
+  5: "Typedefs"
 };
 
