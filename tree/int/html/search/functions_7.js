1b6e6cc7d2aa87f123d2feeeb348e51f29fc19ba
Travis CI User <travis@example.org>
2020-05-24 10:41:33 +0000

    Deploy code docs to GitHub Pages. Travis build: 77
    Commit: 546b5399272f80a466ed313abb3795b9dc8a523b

--- tree/int/html/search/functions_7.js
+++ tree/int/html/search/functions_7.js
@@ -1,5 +1,11 @@
 var searchData=
 [
   ['writeelem',['WriteElem',['../tree__int_8h.html#a61568db0888c2a7b1a471cf029510702',1,'tree_int.c']]],
-  ['writestdoutelem',['WriteStdoutElem',['../tree__int_8h.html#afb4f67576fa555e9f81d0abca4fb3c2c',1,'tree_int.c']]]
+  ['writeinordertree',['WriteInOrderTree',['../tree__int_8h.html#a42b5cc5e3e751bfbe7694fc868a4bc09',1,'tree_int.c']]],
+  ['writepostordertree',['WritePostOrderTree',['../tree__int_8h.html#acf61af9af85f0f8cf91cd46058fe0740',1,'tree_int.c']]],
+  ['writepreordertree',['WritePreOrderTree',['../tree__int_8h.html#a3655251cb206e51d6420befb36e21b08',1,'tree_int.c']]],
+  ['writestdoutelem',['WriteStdoutElem',['../tree__int_8h.html#afb4f67576fa555e9f81d0abca4fb3c2c',1,'tree_int.c']]],
+  ['writestdoutinordertree',['WriteStdoutInOrderTree',['../tree__int_8h.html#a99faf6420a8a89ba99bf2b9a230e00cd',1,'tree_int.c']]],
+  ['writestdoutpostordertree',['WriteStdoutPostOrderTree',['../tree__int_8h.html#aa4769e6cc262a8c1bbc1fab9df721b91',1,'tree_int.c']]],
+  ['writestdoutpreordertree',['WriteStdoutPreOrderTree',['../tree__int_8h.html#ab7488c3ab947f2bc826c06b15982dde0',1,'tree_int.c']]]
 ];
