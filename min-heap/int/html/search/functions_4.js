f2822536536075d966dcb6662e950a67b26415eb
Travis CI User <travis@example.org>
2020-06-01 10:24:33 +0000

    Deploy code docs to GitHub Pages. Travis build: 91
    Commit: 6ee4571dac82341d2dc871abdd2f203bf15ff8f9

--- min-heap/int/html/search/functions_4.js
+++ min-heap/int/html/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['insertnodeminheap',['InsertNodeMinHeap',['../minheap__int_8h.html#acbed04f772d59fe478fffc6216a1fbf8',1,'minheap_int.c']]],
   ['isemptyheap',['IsEmptyHeap',['../minheap__int_8h.html#a3ddaff76dc2ec64155e3f151f4246d4e',1,'minheap_int.c']]]
 ];
